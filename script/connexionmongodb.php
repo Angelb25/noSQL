25215699f5e74f75df53146f158c28b1bd2e8af3
AngelB25 <a.boissie@lyon.ort.asso.fr>
2025-12-18 11:33:51 +0100

    Initial commit - PHP MongoDB setup

---
+++ script/connexionmongodb.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use MongoDB\Client;
+
+try {
+    $client = new Client("mongodb://localhost:30001");
+    $db = $client->test;
+
+    echo "✅ Connexion MongoDB réussie";
+} catch (Exception $e) {
+    echo "❌ Erreur MongoDB : " . $e->getMessage();
+}
