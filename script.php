e1da8769ff9dbef514a393bcf2818915ee84e5d6
ChefPhoque <iglesiasalexandre3@gmail.com>
2026-01-15 15:50:19 +0100

    Commit fichier avec config.php

--- script.php
+++ script.php
@@ -1,1 +1,55 @@
 <?php
+
+use MongoDB\Client;
+
+include "config.php";
+
+try {
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    );
+} catch (PDOException $e) {
+    die("Erreur connexion : " . $e->getMessage());
+}
+
+insert_sortie($pdo, "test");
+
+function insert_sortie($pdo, $nom_club) {
+
+    $sql = "SELECT s.idsortie, s.nom, s.lieu, s.datedebut, :club AS club
+            FROM sortie s
+            LIMIT 10";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        ':club' => $nom_club
+    ]);
+
+    $resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    header('Content-Type: application/json');
+
+    foreach ($resultats as $ligne) {
+
+        echo json_encode($ligne);
+    }
+}
+
+require __DIR__ . '\noSQL\vendor\autoload.php';
+
+
+
+try {
+    $client = new Client("mongodb://localhost:30001");
+    $db = $client->test;
+
+
+    echo "Connexion MongoDB réussie";
+} catch (Exception $e) {
+    echo "Erreur MongoDB : " . $e->getMessage();
+}
