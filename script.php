db40671e2710b2bf467abb657f388cdf04b1c580
AngelB25 <a.boissie@lyon.ort.asso.fr>
2026-01-29 14:29:43 +0100

    commitsuite

--- script.php
+++ script.php
@@ -1,1 +1,56 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+use MongoDB\Client;
+
+$host = "localhost";
+$port = "5432";
+$dbname = "Asvel";
+$user = "postgres";
+$password = "Enzo.2508";
+
+try {
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    );
+} catch (PDOException $e) {
+    die("Erreur connexion : " . $e->getMessage());
+}
+
+insert_sortie($pdo, "test");
+
+function insert_sortie($pdo, $nom_club) {
+
+    $sql = "SELECT s.idsortie, s.nom, s.lieu, s.datedebut, :club AS club
+            FROM sortie s
+            LIMIT 10";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        ':club' => $nom_club
+    ]);
+
+    $resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    header('Content-Type: application/json');
+    
+    $client = new Client("mongodb://localhost:30001");
+    $collection = $client->test->adherent;
+
+    foreach ($resultats as $ligne) {
+        $collection->insertOne($ligne); 
+    }
+
+    try {
+        
+        $db = $client->test;
+
+        echo "Connexion MongoDB réussie";
+    } catch (Exception $e) {
+        echo "Erreur MongoDB : " . $e->getMessage();
+    }
+}
